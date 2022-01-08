b59d0aebe630eb85b7c353e115d207f5b1923bd1
Abraham Bisrat <abrahammehari0@gmail.com>
2022-01-08 08:52:05 -0600

    Made it a complete game logic

--- scripts.js
+++ scripts.js
@@ -2,43 +2,76 @@
 // At the beginning it was all empty, and Abraham Happened lol
 
 
+
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+const player = "player";
+const computer = "computer";
+const maxScore = 3; // You might want this to be an input from the user later on
+
 let randomPick = () => Math.floor(Math.random() * 3) + 1;
 let cPick;
+let playerSelection;
+
+function humanPlay(){
+    playerSelection = rock;
+    // more to come, since we are gonna be asking the user for input later.
+    return playerSelection;
+}
 
 function computerPlay(){
-    cPick = randomPick();
-    let choice = cPick === 1 ? "rock" : cPick === 2 ? "paper" : cPick === 3 ?  "scissors" : "error";
-    console.log("C : " + choice);
-    return choice;
+    let randNum = randomPick();
+    cPick = randNum === 1 ? "rock" : randNum === 2 ? "paper" : randNum === 3 ?  "scissors" : "error";
+    return cPick;
 }
 
-let playerSelection = "scissors";
-console.log("H : " + playerSelection);
 
 function computeWinner(player, computer){
+    player = player.toLowerCase();
 
     if(player === computer) return;
-    else if(player === "rock" && computer === "scissors") return true;
-    else if(player === "paper" && computer === "rock") return true;
-    else if(player === "scissors" && computer === "paper") return true;
+    else if(player === rock && computer === scissors) return true;
+    else if(player === paper && computer === rock) return true;
+    else if(player === scissors && computer === paper) return true;
     
-    else if(player === "rock" && computer === "paper") return false;
-    else if(player === "paper" && computer === "scissors") return false;
-    else if(player === "scissors" && computer === "rock") return false;
+    else if(player === rock && computer === paper) return false;
+    else if(player === paper && computer === scissors) return false;
+    else if(player === scissors && computer === rock) return false;
 }
 
-function whoWon(){
-    let winner = computeWinner(playerSelection, computerPlay());
-    
-    if(winner){
-        console.log("\n Human Won!");
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    let finalWinner;
+
+    while(computerScore < maxScore && playerScore < maxScore){
+        let currentWinner = computeWinner(humanPlay(), computerPlay());
+        if(currentWinner) playerScore++;
+        else if(currentWinner === false) computerScore++;
+        if(playerScore === maxScore) finalWinner = player;
+        else if(computerScore === maxScore) finalWinner = computer;
+        console.log("H : " + playerSelection);
+        console.log("C : " + cPick);
+        currentWinner === true ? console.log("player Won!") : currentWinner === false ? console.log("Computer Won! ") : console.log("Draw!")
+        console.log("H : " + playerScore + "\t C: " + computerScore);
+        console.log(" \n");
     }
-    else if(winner === false){
-        console.log("\n Computer Won!");
+    displayWinner(finalWinner);
+}
+
+function displayWinner(winner){
+    if(winner === player){
+        console.log(`\n You Won! ${playerSelection.toUpperCase()} beats ${cPick.toUpperCase()} !`);
+    }
+    else if(winner === computer){
+        console.log(`\n You Lose! ${cPick.toUpperCase()} beats ${playerSelection.toUpperCase()} !`);
     }
     else{
-        console.log("\n Draw occured!");
+        console.log(`\n SAME TOOLS => ${cPick.toUpperCase()}`)
     }
 }
 
-whoWon();
+
+game();
+
